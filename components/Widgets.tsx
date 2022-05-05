5001be98edc3a121414f9020d8f2478ad4f1389a
Brian-Weloba <bweloba@gmail.com>
2022-05-05 18:18:31 +0300

    Add twittwe widget

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,14 +1,27 @@
 import { SearchCircleIcon, SearchIcon } from '@heroicons/react/outline'
 import React from 'react'
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
 
 function Widgets() {
   return (
-    <div>
+    <div className="mt-2 px-2">
       {/* Searchbox */}
-      <div className="flex items-center space-x-2 bg-gray-100">
-        <SearchIcon className="h-5 w-5" />
-        <input type="text" placeholder="Search Twitter" className="bg-transparent" />
+      <div
+        className="mt-2 flex items-center space-x-2 rounded-full bg-gray-100 
+      p-3"
+      >
+        <SearchIcon className="h-5 w-5 text-gray-400" />
+        <input
+          type="text"
+          placeholder="Search Twitter"
+          className="flex-1 bg-transparent outline-none"
+        />
       </div>
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="ElonJet"
+        options={{ height: 400 }}
+      />
     </div>
   )
 }
