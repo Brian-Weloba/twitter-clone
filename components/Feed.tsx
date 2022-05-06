e97c9c162d055b866204d5903ebb2120bbad294d
Brian-Weloba <bweloba@gmail.com>
2022-05-06 08:56:55 +0300

    Add tweets to feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,10 +1,16 @@
 import { RefreshIcon } from '@heroicons/react/outline'
 import React from 'react'
+import { Tweet } from '../typings'
 import TweetBox from './TweetBox'
+import TweetComponent from './Tweet'
 
-function Feed() {
+interface Props {
+  tweets: Tweet[]
+}
+
+function Feed({ tweets }: Props) {
   return (
-    <div className="col-span-7 lg:col-span-5 border-x">
+    <div className="col-span-7 border-x lg:col-span-5">
       <div className="flex items-center justify-between ">
         <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
         <RefreshIcon
@@ -15,7 +21,13 @@ function Feed() {
       </div>
       {/* Tweetbox */}
       <div>
-          <TweetBox/>
+        <TweetBox />
+      </div>
+      {/* Tweets */}
+      <div>
+        {tweets.map((tweet) => (
+          <TweetComponent key={tweet._id} tweet={tweet} />
+        ))}
       </div>
     </div>
   )
